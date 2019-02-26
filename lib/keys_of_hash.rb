66fce3f8ac7d1851e79be949dc35d1d05d7dc017
Darcy Linde <lindedarcy@gmail.com>
2019-02-26 19:47:32 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
   def keys_of(*arguments)
     new_array = []
     
-    self.each do |key,value|
+    each do |key,value|
       if arguments.include?(value)
         new_array << key
       end
